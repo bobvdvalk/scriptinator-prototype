29afdfd2f2ed11cedf741751a7acd86c86278239
Thomas Biesaart <thomas.biesaart@gmail.com>
2018-01-27 14:18:04 +0100

    Start setting up the model

---
+++ scriptinator-data/src/main/java/io/chapp/scriptinator/model/User.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2018 Thomas Biesaart (thomas.biesaart@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.chapp.scriptinator.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class User extends AbstractEntity {
+    @Pattern(regexp = "[\\w.-]{4,}")
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    private String displayName;
+    private String email;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        if (StringUtils.isEmpty(displayName)) {
+            return username;
+        }
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
