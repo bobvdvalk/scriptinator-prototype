de3df1ae749e3e76fd5e251e20e53fb0d0be5fc5
Bob van der Valk <info@mawoo.nl>
2018-02-02 17:43:53 +0100

    add user rest point

---
+++ scriptinator-rest-api/src/main/java/io/chapp/scriptinator/controller/OwnerController.java
@@ -0,0 +1,27 @@
+package io.chapp.scriptinator.controller;
+
+import io.chapp.scriptinator.model.OwnerDtos;
+import io.chapp.scriptinator.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class OwnerController {
+    private final Logger logger = LoggerFactory.getLogger(OwnerController.class);
+    private final UserService userService;
+
+    public OwnerController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public OwnerDtos ownerDtos(@RequestParam("username") String username) {
+        return OwnerDtos.convert(userService.getByUsername(username));
+    }
+}
