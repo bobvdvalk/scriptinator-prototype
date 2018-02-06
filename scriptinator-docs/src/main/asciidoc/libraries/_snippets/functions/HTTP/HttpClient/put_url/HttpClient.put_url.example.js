cedf6721fed2d7b6c9984a0faed54d5b11fb5846
Luca Scalzotto <luca@scorpiac.com>
2018-02-06 21:37:10 +0100

    Document put_url

---
+++ scriptinator-docs/src/main/asciidoc/libraries/_snippets/functions/HTTP/HttpClient/put_url/HttpClient.put_url.example.js
@@ -0,0 +1,24 @@
+// tag::docs[]
+var HTTP = Script.library("HTTP");      // <1>
+
+var response = HTTP.put(                // <2>
+    "https://httpbin.org/anything"
+);
+
+// end::docs[]
+
+var Assert = Script.library("Assert");
+
+var body = response.body().asJson();
+
+Assert.equal(
+    "No body was sent",
+    "",
+    body.data
+);
+
+Assert.equal(
+    "Content-Length header was sent",
+    "0",
+    body.headers['Content-Length']
+);
