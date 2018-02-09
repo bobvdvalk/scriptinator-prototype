7c0c7d433cef3d623077b2cd0358f726022c5cb2
Thomas Biesaart <thomas.biesaart@gmail.com>
2018-02-09 20:26:30 +0100

    Add more tests

--- scriptinator-rest-api/src/test/java/io/chapp/scriptinator/controller/ScriptControllerTest.java
+++ scriptinator-rest-api/src/test/java/io/chapp/scriptinator/controller/ScriptControllerTest.java
@@ -1,10 +1,27 @@
-package controller;
+/*
+ * Copyright © 2018 Thomas Biesaart (thomas.biesaart@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.chapp.scriptinator.controller;
 
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.ReadContext;
 import io.chapp.scriptinator.model.Project;
+import io.chapp.scriptinator.model.Script;
 import io.chapp.scriptinator.model.User;
 import io.chapp.scriptinator.repositories.ProjectRepository;
+import io.chapp.scriptinator.repositories.ScriptRepository;
 import io.chapp.scriptinator.repositories.UserRepository;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,17 +35,18 @@ import java.io.IOException;
 import static org.testng.AssertJUnit.assertEquals;
 
 @Listeners(ScriptinatorTestCase.class)
-public class UserControllerTest {
+public class ScriptControllerTest {
     private final OkHttpClient client = new OkHttpClient();
 
     @Autowired
+    ScriptRepository scriptRepository;
+    @Autowired
     UserRepository userRepository;
-
     @Autowired
     ProjectRepository projectRepository;
 
     @Test
-    public void testWhenUserIsRequestedItIsReturned() throws IOException {
+    public void testWhenScriptIsRequestedItReturned() throws IOException {
         // Precondition
         User user = new User();
         user.setDisplayName("UserTestUser");
@@ -37,11 +55,25 @@ public class UserControllerTest {
         user.setPassword("thisistest");
         userRepository.save(user);
 
+        Project project = new Project();
+        project.setName("HelloWorld");
+        project.setDescription("This is a description.");
+        project.setOwner(user);
+        projectRepository.save(project);
+
+        Script script = new Script();
+        script.setProject(project);
+        script.setCode("Script.info('Hello World');");
+        script.setName("greet");
+        script.setDescription("Prints hello world.");
+        scriptRepository.save(script);
+
+
         // Action
         Response response = client.newCall(
                 new Request.Builder()
                         .get()
-                        .url("http://localhost:8080/users/Superusertest93")
+                        .url("http://localhost:8080/scripts/" + script.getId())
                         .build()
         ).execute();
 
@@ -49,28 +81,28 @@ public class UserControllerTest {
         ReadContext json = JsonPath.parse(response.body().string());
 
         assertEquals(
-                json.read("$.displayName"),
-                "UserTestUser"
+                json.read("$.name"),
+                "greet"
         );
 
         assertEquals(
-                json.read("$.email"),
-                "testuser@evil.incoperated"
+                json.read("$.code"),
+                "Script.info('Hello World');"
         );
 
         assertEquals(
-                json.read("$.url"),
-                "http://localhost:8080/users/Superusertest93"
+                json.read("$.project.name"),
+                "HelloWorld"
         );
 
         assertEquals(
-                json.read("$.avatarUrl"),
-                "https://www.gravatar.com/avatar/b18d01a30f85969ec426927c7453c5ea"
+                json.read("$.project.owner.username"),
+                "Superusertest93"
         );
     }
 
     @Test
-    public void testWhenProjectListIsEmpty() throws IOException {
+    public void testWhenScriptListIsRequestedItReturned() throws IOException {
         // Precondition
         User user = new User();
         user.setDisplayName("UserTestUser");
@@ -79,11 +111,25 @@ public class UserControllerTest {
         user.setPassword("thisistest");
         userRepository.save(user);
 
+        Project project = new Project();
+        project.setName("HelloWorld");
+        project.setDescription("This is a description.");
+        project.setOwner(user);
+        projectRepository.save(project);
+
+        Script script = new Script();
+        script.setProject(project);
+        script.setCode("Script.info('Hello World');");
+        script.setName("greet");
+        script.setDescription("Prints hello world.");
+        scriptRepository.save(script);
+
+
         // Action
         Response response = client.newCall(
                 new Request.Builder()
                         .get()
-                        .url("http://localhost:8080/users/Superusertest93/projects")
+                        .url("http://localhost:8080/scripts/")
                         .build()
         ).execute();
 
@@ -91,8 +137,23 @@ public class UserControllerTest {
         ReadContext json = JsonPath.parse(response.body().string());
 
         assertEquals(
+                json.read("$.totalItemCount"),
+                (Integer) 1
+        );
+
+        assertEquals(
+                json.read("$.items.[0].name"),
+                "greet"
+        );
+
+        assertEquals(
+                json.read("$.items.[0].project.name"),
+                "HelloWorld"
+        );
+
+        assertEquals(
                 json.read("$.hasItems"),
-                Boolean.valueOf("false")
+                Boolean.valueOf("true")
         );
 
         assertEquals(
@@ -107,7 +168,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testWithProjects() throws IOException {
+    public void scriptReturnEmptyScriptList() throws IOException {
         // Precondition
         User user = new User();
         user.setDisplayName("UserTestUser");
@@ -126,16 +187,21 @@ public class UserControllerTest {
         Response response = client.newCall(
                 new Request.Builder()
                         .get()
-                        .url("http://localhost:8080/users/Superusertest93/projects")
+                        .url("http://localhost:8080/scripts")
                         .build()
         ).execute();
 
-        //Validation
+        // Validation
         ReadContext json = JsonPath.parse(response.body().string());
 
         assertEquals(
+                json.read("$.totalItemCount"),
+                (Integer) 0
+        );
+
+        assertEquals(
                 json.read("$.hasItems"),
-                Boolean.valueOf("true")
+                Boolean.valueOf("false")
         );
 
         assertEquals(
@@ -144,8 +210,9 @@ public class UserControllerTest {
         );
 
         assertEquals(
-                json.read("$.totalItemCount"),
-                (Integer) 1
+                json.read("$.hasPrevious"),
+                Boolean.valueOf("false")
         );
+
     }
 }
--- scriptinator-rest-api/src/test/java/io/chapp/scriptinator/controller/UserControllerTest.java
+++ scriptinator-rest-api/src/test/java/io/chapp/scriptinator/controller/UserControllerTest.java
@@ -1,12 +1,25 @@
-package controller;
+/*
+ * Copyright © 2018 Thomas Biesaart (thomas.biesaart@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.chapp.scriptinator.controller;
 
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.ReadContext;
 import io.chapp.scriptinator.model.Project;
-import io.chapp.scriptinator.model.Script;
 import io.chapp.scriptinator.model.User;
 import io.chapp.scriptinator.repositories.ProjectRepository;
-import io.chapp.scriptinator.repositories.ScriptRepository;
 import io.chapp.scriptinator.repositories.UserRepository;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,18 +33,17 @@ import java.io.IOException;
 import static org.testng.AssertJUnit.assertEquals;
 
 @Listeners(ScriptinatorTestCase.class)
-public class ScriptControllerTest {
+public class UserControllerTest {
     private final OkHttpClient client = new OkHttpClient();
 
     @Autowired
-    ScriptRepository scriptRepository;
-    @Autowired
     UserRepository userRepository;
+
     @Autowired
     ProjectRepository projectRepository;
 
     @Test
-    public void testWhenScriptIsRequestedItReturned() throws IOException {
+    public void testWhenUserIsRequestedItIsReturned() throws IOException {
         // Precondition
         User user = new User();
         user.setDisplayName("UserTestUser");
@@ -40,25 +52,11 @@ public class ScriptControllerTest {
         user.setPassword("thisistest");
         userRepository.save(user);
 
-        Project project = new Project();
-        project.setName("HelloWorld");
-        project.setDescription("This is a description.");
-        project.setOwner(user);
-        projectRepository.save(project);
-
-        Script script = new Script();
-        script.setProject(project);
-        script.setCode("Script.info('Hello World');");
-        script.setName("greet");
-        script.setDescription("Prints hello world.");
-        scriptRepository.save(script);
-
-
         // Action
         Response response = client.newCall(
                 new Request.Builder()
                         .get()
-                        .url("http://localhost:8080/scripts/" + script.getId())
+                        .url("http://localhost:8080/users/Superusertest93")
                         .build()
         ).execute();
 
@@ -66,28 +64,65 @@ public class ScriptControllerTest {
         ReadContext json = JsonPath.parse(response.body().string());
 
         assertEquals(
-                json.read("$.name"),
-                "greet"
+                json.read("$.displayName"),
+                "UserTestUser"
         );
 
         assertEquals(
-                json.read("$.code"),
-                "Script.info('Hello World');"
+                json.read("$.email"),
+                "testuser@evil.incoperated"
         );
 
         assertEquals(
-                json.read("$.project.name"),
-                "HelloWorld"
+                json.read("$.url"),
+                "http://localhost:8080/users/Superusertest93"
         );
 
         assertEquals(
-                json.read("$.project.owner.username"),
-                "Superusertest93"
+                json.read("$.avatarUrl"),
+                "https://www.gravatar.com/avatar/b18d01a30f85969ec426927c7453c5ea"
         );
     }
 
     @Test
-    public void testWhenScriptListIsRequestedItReturned() throws IOException {
+    public void testWhenProjectListIsEmpty() throws IOException {
+        // Precondition
+        User user = new User();
+        user.setDisplayName("UserTestUser");
+        user.setEmail("testuser@evil.incoperated");
+        user.setUsername("Superusertest93");
+        user.setPassword("thisistest");
+        userRepository.save(user);
+
+        // Action
+        Response response = client.newCall(
+                new Request.Builder()
+                        .get()
+                        .url("http://localhost:8080/users/Superusertest93/projects")
+                        .build()
+        ).execute();
+
+        // Validation
+        ReadContext json = JsonPath.parse(response.body().string());
+
+        assertEquals(
+                json.read("$.hasItems"),
+                Boolean.valueOf("false")
+        );
+
+        assertEquals(
+                json.read("$.hasNext"),
+                Boolean.valueOf("false")
+        );
+
+        assertEquals(
+                json.read("$.hasPrevious"),
+                Boolean.valueOf("false")
+        );
+    }
+
+    @Test
+    public void testWithProjects() throws IOException {
         // Precondition
         User user = new User();
         user.setDisplayName("UserTestUser");
@@ -102,39 +137,16 @@ public class ScriptControllerTest {
         project.setOwner(user);
         projectRepository.save(project);
 
-        Script script = new Script();
-        script.setProject(project);
-        script.setCode("Script.info('Hello World');");
-        script.setName("greet");
-        script.setDescription("Prints hello world.");
-        scriptRepository.save(script);
-
-
         // Action
         Response response = client.newCall(
                 new Request.Builder()
                         .get()
-                        .url("http://localhost:8080/scripts/")
+                        .url("http://localhost:8080/users/Superusertest93/projects")
                         .build()
         ).execute();
 
-        // Validation
+        //Validation
         ReadContext json = JsonPath.parse(response.body().string());
-
-        assertEquals(
-                json.read("$.totalItemCount"),
-                (Integer) 1
-        );
-
-        assertEquals(
-                json.read("$.items.[0].name"),
-                "greet"
-        );
-
-        assertEquals(
-                json.read("$.items.[0].project.name"),
-                "HelloWorld"
-        );
 
         assertEquals(
                 json.read("$.hasItems"),
@@ -147,57 +159,8 @@ public class ScriptControllerTest {
         );
 
         assertEquals(
-                json.read("$.hasPrevious"),
-                Boolean.valueOf("false")
-        );
-    }
-
-    @Test
-    public void scriptReturnEmptyScriptList() throws IOException {
-        // Precondition
-        User user = new User();
-        user.setDisplayName("UserTestUser");
-        user.setEmail("testuser@evil.incoperated");
-        user.setUsername("Superusertest93");
-        user.setPassword("thisistest");
-        userRepository.save(user);
-
-        Project project = new Project();
-        project.setName("HelloWorld");
-        project.setDescription("This is a description.");
-        project.setOwner(user);
-        projectRepository.save(project);
-
-        // Action
-        Response response = client.newCall(
-                new Request.Builder()
-                        .get()
-                        .url("http://localhost:8080/scripts")
-                        .build()
-        ).execute();
-
-        // Validation
-        ReadContext json = JsonPath.parse(response.body().string());
-
-        assertEquals(
                 json.read("$.totalItemCount"),
-                (Integer) 0
+                (Integer) 1
         );
-
-        assertEquals(
-                json.read("$.hasItems"),
-                Boolean.valueOf("false")
-        );
-
-        assertEquals(
-                json.read("$.hasNext"),
-                Boolean.valueOf("false")
-        );
-
-        assertEquals(
-                json.read("$.hasPrevious"),
-                Boolean.valueOf("false")
-        );
-
     }
 }
